636731100e2c7bc1034d7cc97eb1cef96b5c321a
Santunis Monteiro de Oliveira <santunis.oliveira@fornecedores.sicoob.com.br>
2020-07-08 00:12:59 -0300

    Cliente - Exibindo Pessoas e Produtos

--- login-app/src/app/main/people/people.component.ts
+++ login-app/src/app/main/people/people.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from '../main.service';
+import { Observable } from 'rxjs';
+import { Person } from '../person';
 
 @Component({
   selector: 'app-people',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleComponent implements OnInit {
 
-  constructor() { }
+  people$: Observable<Person[]>;
+
+  constructor(
+    private mainService: MainService
+  ) { }
 
   ngOnInit(): void {
+    this.people$ = this.mainService.getPeople();
   }
 
 }
--- login-app/src/app/main/products/products.component.ts
+++ login-app/src/app/main/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from '../main.service';
+import { Observable } from 'rxjs';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products$: Observable<Product[]>;
+
+  constructor(
+    private mainService: MainService
+  ) { }
 
   ngOnInit(): void {
+    this.products$ = this.mainService.getProducts();
   }
 
 }
